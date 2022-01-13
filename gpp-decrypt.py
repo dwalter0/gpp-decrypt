fc5c64e532c8a01a7926157d80c6a23085a09c04
dwalter0 <ds_walters@hotmail.com>
2022-01-13 13:35:26 +1100

    Update gpp-decrypt.py

--- gpp-decrypt.py
+++ gpp-decrypt.py
@@ -65,6 +65,6 @@ def main():
 
 
 if __name__ == "__main__":
-    os.system('cls' if os.name == 'nt' else 'clear')
+    #os.system('cls' if os.name == 'nt' else 'clear')
     print(banner)
     main()
